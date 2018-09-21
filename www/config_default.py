d34fd06143b0f852c3bae9fd3bf9e805e5c9002b
bao <yuzi0915@gmail.com>
2018-09-21 22:19:26 +0800

    add config

---
+++ www/config_default.py
@@ -0,0 +1,17 @@
+'''
+Default configurations.
+'''
+
+configs = {
+    'debug':True,
+    'db':{
+        'host':'192.168.181.128',
+        'port':3306,
+        'user':'root',
+        'password':'zjbaaa',
+        'db':'awesome'
+    },
+    'session':{
+        'secret':'Awesome'
+    }
+}
